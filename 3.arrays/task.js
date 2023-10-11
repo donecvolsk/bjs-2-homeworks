ef621bcf8a1849288d8b471bbce898c61c7f6b09
donecvolsk <donecvolsk@yandex.ru>
2023-10-11 12:28:58 +0400

    Третье задание полностью готово

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,15 @@
 function compareArrays(arr1, arr2) {
-  
+    return arr1.length === arr2.length && arr1.every((item, i) => item === arr2[i]);
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+    let results = users.filter((items) => items.gender === gender).reduce((accumulator, item, index, array) => {
+        accumulator += item.age;
+        if(index === array.length -1) {
+          return accumulator / array.length;
+        }
+        return accumulator; 
+      }, 0)
+      
+      return results;
 }
