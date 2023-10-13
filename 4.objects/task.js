06c3a74113d293a74ea6357009afa7216a67188c
donecvolsk <donecvolsk@yandex.ru>
2023-10-13 12:41:34 +0400

    Четвертое задание полностью готово

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,34 @@
 function Student(name, gender, age) {
-  
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
+  this.marks = [];
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  
+  this.subject = subjectName;
 }
 
 Student.prototype.addMarks = function (...marks) {
-  
+  if(Array.isArray(this.marks)) {
+    this.marks.push(...marks);
+     }
 }
 
-Student.prototype.getAverage = function () {
-  
+
+Student.prototype.getAverage = function () { 
+  let sum = 0;
+  if(Array.isArray(this.marks)) {
+    for (let i = 0; i < this.marks.length; i++) {
+      sum += this.marks[i] / this.marks.length;
+    }
+  }
+  return sum;
 }
+
 
 Student.prototype.exclude = function (reason) {
-  
+  delete this.subject;
+  delete this.marks;
+  this.excluded = reason;
 }
