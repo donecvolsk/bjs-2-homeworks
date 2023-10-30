c6a0cdd294bbe3c005bcc77c3782374feb6bbfb6
donecvolsk <donecvolsk@yandex.ru>
2023-10-30 08:23:07 +0400

    Седьмое задание полностью готово

---
+++ 7.async/task.js
@@ -0,0 +1,81 @@
+class AlarmClock {
+    constructor() {
+        this.alarmCollection = [];
+        this.intervalId = null;
+    }
+    //Добавляет новый звонок в коллекцию существующих
+    addClock(timeStart, callback) {
+
+        if(timeStart.length === 0 || typeof callback != 'function') {
+            throw new Error('Отсутствуют обязательные аргументы');
+         } 
+        
+        for (let i = 0; i < this.alarmCollection.length; i++) {
+            if(timeStart === this.alarmCollection[i].time){
+                console.warn('Уже присутствует звонок на это же время');
+            }          
+        }
+
+            this.alarmCollection.push({
+            callback: callback,
+            time: timeStart,
+            canCall: true,
+        });
+           
+    }
+    // удаляет звонки по определённому времени
+    removeClock (timeDelete, ) {
+
+        this.alarmCollection = this.alarmCollection.filter(comparisonTime => comparisonTime.time !== timeDelete);
+    }
+
+    //возвращает текущее время в строковом формате HH:MM.
+    getCurrentFormattedTime() {
+        return new Date().toLocaleTimeString().slice(0,-3);
+    }
+
+    //запускает будильник
+    start() {
+
+        if(this.intervalId !== null) {
+         return;
+        }
+
+        this.intervalId = setInterval(() => {
+            this.alarmCollection.forEach(callElement => {
+              if(callElement.time === this.getCurrentFormattedTime() && callElement.canCall === true) {
+                callElement.canCall = false;
+                callElement.callback();
+              }
+            });
+        }, 1000);
+    }
+
+    //останавливает выполнение интервала будильника.
+    stop() {
+
+        clearInterval(this.intervalId);
+        this.intervalId = null;
+    }
+
+    //сбрасывает возможность запуска всех звонков
+    resetAllCalls() {
+
+        this.alarmCollection.forEach(canCallElement => {
+            canCallElement.canCall = true;
+        });
+    }
+
+    //удаляет все звонки
+    clearAlarms() {
+
+        this.stop();
+        this.alarmCollection = [];
+    }
+}
+
+/*let clock;
+clock = new AlarmClock();
+const callback = f => f;
+clock.addClock("16:45", callback);*/
+
